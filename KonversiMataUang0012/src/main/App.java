6f9c81b293e2f114444d52d3b15dfd4e50f58d85
Ayuni Lidyastuti <98303608+issawitch@users.noreply.github.com>
2024-11-28 20:02:00 +0700

    Uploading files

---
+++ KonversiMataUang0012/src/main/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class App {
+    public static void main(String[] args) {
+        KonversiView konversi = new KonversiView();
+        konversi.setVisible(true);
+    }
+}
